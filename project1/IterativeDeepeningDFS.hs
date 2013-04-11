c5530461b0007210243af62c83c5b8b8258ae06c
dacto <aliensarereal51@gmail.com>
2013-04-10 23:15:34 -0700

    Ready for incremental depth counter.

--- project1/IterativeDeepeningDFS.hs
+++ project1/IterativeDeepeningDFS.hs
@@ -1,9 +1,9 @@
 module IterativeDeepeningDFS (listM) where
 
-listM :: (Eq a, Monad m) => (a -> m [a]) -> a -> m [a]
-listM f x = return $ listM' f x 1
+listM :: (Eq a, Eq b, Monad m) => ((a,[b]) -> m [(a,[b])]) -> (a,[b]) -> m [(a,[b])]
+listM f x = listM' f x 1
 
-listM' :: (Eq a, Monad m) => (a -> m [a]) -> a -> Int -> m [a]
+listM' :: (Eq a, Eq b, Monad m) => ((a,[b]) -> m [(a,[b])]) -> (a,[b]) -> Int -> m [(a,[b])]
 listM' f x@(_,n) i = do expand <- f x
                         let depth = length n
                         if null expand || depth >= i then return []
