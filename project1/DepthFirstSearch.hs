e01cfbbe30e3b8097584b4dd7517f47036229f7e
dacto <aliensarereal51@gmail.com>
2013-04-10 20:37:16 -0700

    Removed excess '$'

--- project1/DepthFirstSearch.hs
+++ project1/DepthFirstSearch.hs
@@ -3,5 +3,5 @@ module DepthFirstSearch (listM) where
 listM :: (Eq a, Monad m) => (a -> m [a]) -> a -> m [a]
 listM f x = do expand <- f x
                if null expand then return []
-               else do next <- mapM (listM f) $ expand
+               else do next <- mapM (listM f) expand
                        return $ x : concat next
