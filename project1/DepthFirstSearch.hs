fe1bbf9032a593d5b2fce6f3b4c7b6b0f57fc7fb
dacto <aliensarereal51@gmail.com>
2013-04-10 19:54:05 -0700

    Increased laziness in DepthFirstSearch.hs

--- project1/DepthFirstSearch.hs
+++ project1/DepthFirstSearch.hs
@@ -2,11 +2,10 @@ module DepthFirstSearch (listM) where
 
 listM :: (Eq a, Monad m) => (a -> m [a]) -> a -> m [a]
 listM f x = do rest <- listM' f [x]
-               if null rest then return [x]
-               else return $ x : rest
+               return (if null rest then [x] else x : rest)
 
 listM' :: (Eq a, Monad m) => (a -> m [a]) -> [a] -> m [a]
-listM' f []     = return []
+listM' _ []     = return []
 listM' f (x:xs) = do next <- listM' f [x]
                      rest <- listM' f xs
                      return $ x : next ++ rest
