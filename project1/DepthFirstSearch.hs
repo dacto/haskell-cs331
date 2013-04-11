471dbc861e4f542b052dd55de081d8404ee25012
Paul Freeman <freemapa@onid.orst.edu>
2013-04-10 19:51:35 -0700

    Fixed all my errors

--- project1/DepthFirstSearch.hs
+++ project1/DepthFirstSearch.hs
@@ -1,10 +1,12 @@
 module DepthFirstSearch (listM) where
 
 listM :: (Eq a, Monad m) => (a -> m [a]) -> a -> m [a]
-listM f x = do rest <- listM' f x
-               if nulll rest then return x
+listM f x = do rest <- listM' f [x]
+               if null rest then return [x]
                else return $ x : rest
 
 listM' :: (Eq a, Monad m) => (a -> m [a]) -> [a] -> m [a]
 listM' f []     = return []
-listM' f (x:xs) = return $ x : listM' f x ++ listM' f xs
+listM' f (x:xs) = do next <- listM' f [x]
+                     rest <- listM' f xs
+                     return $ x : next ++ rest
