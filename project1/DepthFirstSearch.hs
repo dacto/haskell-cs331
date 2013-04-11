b68ba208a26a837a2053409c058d6b7fa968dbc4
Paul Freeman <freemapa@onid.orst.edu>
2013-04-10 20:36:12 -0700

    It's working NOW

--- project1/DepthFirstSearch.hs
+++ project1/DepthFirstSearch.hs
@@ -1,11 +1,7 @@
 module DepthFirstSearch (listM) where
 
 listM :: (Eq a, Monad m) => (a -> m [a]) -> a -> m [a]
-listM f x = do rest <- listM' f [x]
-               return (if null rest then [x] else x : rest)
-
-listM' :: (Eq a, Monad m) => (a -> m [a]) -> [a] -> m [a]
-listM' _ []     = return []
-listM' f (x:xs) = do next <- listM' f [x]
-                     rest <- listM' f xs
-                     return $ x : next ++ rest
+listM f x = do expand <- f x
+               if null expand then return []
+               else do next <- mapM (listM f) $ expand
+                       return $ x : concat next
