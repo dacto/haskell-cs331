4a5e8a06e3e119c240f66752a461ed44d60df9bf
Paul Freeman <freemapa@onid.orst.edu>
2013-04-13 23:27:49 -0700

    Refactored this search

--- project1/DepthFirstSearch.hs
+++ project1/DepthFirstSearch.hs
@@ -1,18 +1,15 @@
 module DepthFirstSearch (solveM) where
-
 import Data.List (find)
 
 
 
 solveM :: (Eq a, Monad m) => (a -> m [a]) -> (a -> Bool) -> a -> m (Maybe a)
-
 solveM expand isGoal root = do tree <- listM expand root
                                return $ find isGoal tree
 
 
 
 listM :: (Eq a, Monad m) => (a -> m [a]) -> a -> m [a]
-
 listM expand node = do new <- expand node
                        if null new then return []
                        else do rest <- mapM (listM expand) new
