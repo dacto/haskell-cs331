13197d47bf4983adcfd674b88c969ae1c3c0a9e7
Paul Freeman <freemapa@onid.orst.edu>
2013-04-17 00:41:08 -0700

    works but has stack issues

--- project1/IterativeDeepeningDFS.hs
+++ project1/IterativeDeepeningDFS.hs
@@ -1,50 +1,54 @@
 module IterativeDeepeningDFS (solveM) where
+import Data.Set (Set, empty, member, insert)
 import Data.List (find)
 import Control.Monad.Trans.State.Lazy
 
 
 
-solveM :: Eq a=>
+solveM :: Ord a =>
           ((a,[String]) -> [(a,[String])])
           -> ((a,[String]) -> Bool)
           -> (a,[String])
           -> [String]
 solveM expand isGoal root =
-    case runState (solveDepth expand isGoal root 1) (0, 0) of
+    case runState (solveDepth expand isGoal root empty 0) (0, 0) of
     (Nothing, (nodes, depth))         -> ["no solution found"]++[show nodes++" nodes expanded"]++["searched to depth "++show depth]
     (Just (x, moves), (nodes, depth)) -> reverse moves++[show nodes++" nodes expanded"]++["searched to depth "++show depth]
 
 
 
-solveDepth :: Eq a =>
+solveDepth :: Ord a =>
               ((a,[String]) -> [(a,[String])])
               -> ((a,[String]) -> Bool)
               -> (a,[String])
+              -> Set a
               -> Int
               -> State (Int, Int) (Maybe (a,[String]))
-solveDepth expand isGoal root searchDepth =
-    do tree <- listM expand searchDepth root
+solveDepth expand isGoal root set searchDepth =
+    do tree <- listM expand searchDepth set root
        (nodeCount, treeDepth) <- get
        let goal = find isGoal tree
        case goal of
             Nothing -> if searchDepth > treeDepth then return Nothing
-                       else do nextDepth <- solveDepth expand isGoal root $ searchDepth+1
+                       else do nextDepth <- solveDepth expand isGoal root set $ searchDepth+1
                                return nextDepth
             _       -> return goal
 
 
 
-listM :: Eq a =>
+listM :: Ord a =>
          ((a,[String]) -> [(a,[String])])
          -> Int
+         -> Set a
          -> (a,[String])
          -> State (Int, Int) [(a,[String])]
-listM expand searchDepth node@(state, moves) =
-    do let currDepth = length moves
-       if currDepth > searchDepth then return []
-       else do let children = expand node
-               if null children then return []
-               else do (nodeCount, treeDepth) <- get
-                       put (nodeCount+1, max currDepth treeDepth)
-                       rest <- mapM (listM expand searchDepth) children
+listM expand searchDepth set node@(state, moves) =
+    do let currDepth = length moves - 1
+       if currDepth >= searchDepth || member state set then return [node]
+       else do (nodeCount, treeDepth) <- get
+               put (nodeCount + 1, max (currDepth + 1) treeDepth)
+               let children = expand node
+               let newSet = insert state set
+               if null children then return [node]
+               else do rest <- mapM (listM expand searchDepth newSet) children
                        return $ node : concat rest
