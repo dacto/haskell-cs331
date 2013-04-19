f0e9c57e9294e6fdfcd439182b9bb597151b4a1a
Paul Freeman <freemapa@onid.orst.edu>
2013-04-18 18:20:10 -0700

    Added Comments

--- project1/DepthFirstSearch.hs
+++ project1/DepthFirstSearch.hs
@@ -1,16 +1,17 @@
+-- Depth-First Search
 module DepthFirstSearch (solveM) where
 import Data.List (find)
 
-
-
+--     This is the depth first search algorithm. Very simply put,
+--     it recurses down the left side of the tree. Upon finding
+--     a goal node or an invalid node, it stops recursion and returns.
 solveM :: (Eq a, Monad m) => (a -> m [a]) -> (a -> Bool) -> a -> m (Maybe a)
 solveM expand isGoal root = do tree <- listM expand root
                                return $ find isGoal tree
-
-
 
 listM :: (Eq a, Monad m) => (a -> m [a]) -> a -> m [a]
 listM expand node = do new <- expand node
                        if null new then return []
                        else do rest <- mapM (listM expand) new
                                return $ node : concat rest
+
