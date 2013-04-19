637d7222f02b4be1f9c70545a24585a7eca2e551
Paul Freeman <freemapa@onid.orst.edu>
2013-04-18 17:55:05 -0700

    Added comments

--- project1/BreadthFirstSearch.hs
+++ project1/BreadthFirstSearch.hs
@@ -1,6 +1,11 @@
+-- Breadth First Search
 module BreadthFirstSearch (solveM) where
 import Data.List (find)
 
+--     This is the primary code for breadth-first search.
+--     It operates by continually expanding deeper rows of nodes,
+--     supplying an infinite list of nodes. Find is called on
+--     this list to locate the first node matching the goal.
 solveM :: (Eq a, Functor m, Monad m) => (a -> m [a]) -> (a -> Bool) -> a -> m (Maybe a)
 solveM expand isGoal root = fmap (find isGoal) (listM expand [root])
 
@@ -10,10 +15,3 @@ listM expand row = do next <- mapM expand row
                       else do rest <- listM expand $ concat next
                               return $ row ++ rest
 
-							--do tree <- listM expand root
-                            --   return $ find isGoal tree
-
---listM :: (Eq a, Monad m) => (a -> m [a]) -> a -> m [a]
---listM expand node = do new <- expand node
---                       rest <- listM' expand new
---                       return $ node : rest
